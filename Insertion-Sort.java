a9a6a54e7d062630bf47a4d3f40e88697312f1ed
Mohamed Anas <smohamedanas02@gmail.com>
2026-07-25 12:31:54 +0530

    Create Insertion-Sort.java

---
+++ Insertion-Sort.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    int arr[]={5,6,7,3,43,67};
+		for(int i=1;i<arr.length;i++){
+		    int key=arr[i];
+		    int j=i-1;
+		    while(j>=0 && arr[j]>key){
+		        arr[j+1]=arr[j];
+		        j--;
+		    }
+		    arr[j+1]=key;
+		}
+		System.out.println(Arrays.toString(arr));
+	}
+}
